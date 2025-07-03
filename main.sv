0a93b9f912835da7860a6226072d8e101d89f6cb
Julio Nunes Avelar <jn5.guape@gmail.com>
2025-07-03 16:59:19 -0300

    Atualizando teste

--- main.sv
+++ main.sv
@@ -1,17 +1,43 @@
 module top (
     input  logic clk,
-    input  logic rst_n,
+    input  logic rst,
     
-    output logic [7:0] leds,
+    output logic [7:0] led,
     output logic builtin_led,
 
-    input  logic tx,
-    output logic rx
+    output logic tx,
+    input  logic rx
 );
 
-    localparam SECOND = 25_000_000;
+    localparam SECOND    = 25_000_000;
+    localparam CLK_FREQ  = 25_000_000;
+    localparam BAUD_RATE = 115_200;
 
     logic [31:0] counter;
+    logic rst_n;
+    logic [1:0] state;
+
+    initial begin
+        state = 0;
+        rst_n = 0;
+    end
+
+    always @(posedge clk) begin
+        case(state)
+            0: begin
+                state <= 1;
+                rst_n <= 0;
+            end
+            1: begin
+                state <= 2;
+                rst_n <= 0;
+            end
+            2: begin
+                state <= 2;
+                rst_n <= 1;
+            end
+        endcase
+    end
 
     initial begin
         counter     = 0;
@@ -22,8 +48,8 @@ module top (
         if(counter < SECOND) begin
             counter <= counter + 1;
         end else begin
-            led     <= ~led;
-            counter <= 0;
+            builtin_led <= ~builtin_led;
+            counter     <= 0;
         end
     end
 
@@ -31,14 +57,14 @@ module top (
     logic [7:0] uart_rx_data;
 
     UART_TX #(
-        .BAUD_RATE  (115_200),
-        .CLK_FREQ   (25_000_000)
+        .BAUD_RATE       (BAUD_RATE),
+        .CLK_FREQ        (CLK_FREQ)
     ) uart_tx (
         .clk             (clk),
-        .rst_n           (1),
+        .rst_n           (rst_n),
 
         .wr_bit_period_i (0),
-        .bit_period_i    (115_200),
+        .bit_period_i    (CLK_FREQ / BAUD_RATE),
 
         .parity_type_i   (1),
         .uart_tx_en      (uart_rx_valid),
@@ -48,14 +74,14 @@ module top (
     );
 
     UART_RX #(
-        .BAUD_RATE              (115_200),
-        .CLK_FREQ               (25_000_000)
+        .BAUD_RATE              (BAUD_RATE),
+        .CLK_FREQ               (CLK_FREQ)
     ) uart_rx (
         .clk                    (clk),
-        .rst_n                  (1),
+        .rst_n                  (rst_n),
 
         .wr_bit_period_i        (0),
-        .bit_period_i           (115200),
+        .bit_period_i           (CLK_FREQ / BAUD_RATE),
 
         .parity_type_i          (1),
         .uart_rxd               (rx),
